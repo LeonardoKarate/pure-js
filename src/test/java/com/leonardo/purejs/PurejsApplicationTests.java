b702ce504f04cc6551159e8293ec2082e2f53cd8
Leonardo_Nunes <leonardokarate95@hotmail.com>
2023-04-27 11:39:11 -0300

    commit inicial

---
+++ src/test/java/com/leonardo/purejs/PurejsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.purejs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurejsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
